cb54ba70076118e06aadc6c9a440c905c7f86503
anirudhkhabya <ianirudhkhabya@gmail.com>
2024-05-15 01:36:50 +0530

    feat: insert video controls

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,24 +1,24 @@
 "use client";
 
 import React, { useRef, useState } from "react";
-import { FaPause, FaPlay } from "react-icons/fa";
+// import { FaPause, FaPlay } from "react-icons/fa";
 
 import Content from "@/components/Content";
 
 const PlayerPage = () => {
-  const [isPaused, setIsPaused] = useState(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
+  // const [isPaused, setIsPaused] = useState(false);
+  // const videoRef = useRef<HTMLVideoElement>(null);
 
-  const togglePlayPause = () => {
-    if (videoRef.current) {
-      if (isPaused) {
-        videoRef.current.play();
-      } else {
-        videoRef.current.pause();
-      }
-      setIsPaused(!isPaused);
-    }
-  };
+  // const togglePlayPause = () => {
+  //   if (videoRef.current) {
+  //     if (isPaused) {
+  //       videoRef.current.play();
+  //     } else {
+  //       videoRef.current.pause();
+  //     }
+  //     setIsPaused(!isPaused);
+  //   }
+  // };
 
   return (
     <Content>
@@ -26,13 +26,13 @@ const PlayerPage = () => {
         <div className="flex flex-col items-center justify-center h-full">
           <h1 className="text-2xl font-bold">Player</h1>
           <div className="relative w-[50%] max-w-6xl mx-auto my-4 rounded-xl overflow-hidden">
-            <button
+            {/* <button
               onClick={togglePlayPause}
               className="absolute top-4 right-4 z-10 text-white"
             >
               {isPaused ? <FaPlay size={20} /> : <FaPause size={20} />}
-            </button>
-            <video className="w-full" ref={videoRef} loop muted autoPlay>
+            </button> */}
+            <video className="w-full" controls loop muted autoPlay>
               <source src="/video.mp4" />
             </video>
           </div>
